3d5e5b57609c7d0ac5eb699aaa2f5405f2fb519d
Natália Marques Pires <155389787+NataliaMarquesPires@users.noreply.github.com>
2025-02-02 20:25:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using ControleDeContatos.Data;
+using ControleDeContatos.Repositorio;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeCadastro {
+    public class Program {
+
+        public static void Main(string[] args) {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            //Configure do DbContext
+            builder.Services.AddDbContext<BancoContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
+            builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
